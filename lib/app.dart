3184ec0fd9d73012a697faedc430b4fc4dbeffb0
AmanCH3 <armanachaudhary53@gmail.com>
2025-05-04 19:32:10 +0545

    feat : Layout view for the dashbaord

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:first_program/dashboard_view.dart';
+import 'package:first_program/view/layout_view.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -7,7 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DashboardView(),
+      home: LayoutView(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/view/layout_view.dart
+++ lib/view/layout_view.dart
@@ -6,52 +6,43 @@ class LayoutView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      body :Container(
-        color:  Colors.black38,
-        width:  double.infinity,
-
+      body: Container(
+        color: Colors.white,
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(vertical: 50.0),
         child: Row(
-
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Column(
-
-              children: [
-                // ElevatedButton.icon()
-                Container(
-                  //Text
-
-                )
-              ],
-
-            ) ,
-
-            Column(
-              children: [
-                //ElevatedButton.icon()
-
-                Container(
-                  //Text
-                )
-              ],
-
-
-            ) ,
-            Column(
-
-              children: [
-                //ElevatedButton.icon()
-
-                Container(
-                  //Text
-                )
-              ],
-
-            )
+            buildColumn(Icons.call, "Call", iconColor: Colors.black),
+            buildColumn(Icons.near_me, "Route"),
+            buildColumn(Icons.share, "Share"),
           ],
         ),
-      )
-    ) ;
+      ),
+    );
   }
+}
+
+Widget buildColumn(IconData icon, String label, {Color iconColor = Colors.teal}) {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      Icon(icon, color: iconColor),
+      const SizedBox(height: 8),
+      Container(
+        // This ensures all containers have the same constraints
+        constraints: const BoxConstraints(minWidth: 40),
+        child: Text(
+          label,
+          textAlign: TextAlign.center, // Center text for better alignment
+          style: const TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            color: Colors.red,
+          ),
+        ),
+      ),
+    ],
+  );
 }
